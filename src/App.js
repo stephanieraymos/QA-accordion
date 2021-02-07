1a524445cf9a6a545b85e1c0478eaf78810617b5
Stephanie Raymos <stephanieraymos@gmail.com>
2021-02-06 20:12:34 -0800

    Up and running

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data)
+
+  return <> <div>Hi</div>
+  </>;
 }
 
 export default App;
