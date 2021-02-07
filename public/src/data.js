9548da9683d9aef5149688a3e1708fa72d5fbd77
Stephanie Raymos <stephanieraymos@gmail.com>
2021-02-06 19:58:04 -0800

    Questions and Answers data

---
+++ public/src/data.js
@@ -0,0 +1,33 @@
+const questions = [
+  {
+    id: 1,
+    title: 'How much would wood a woodchuck chuck if a woodchuck could chuck wood?',
+    info:
+      'All the wood.',
+  },
+  {
+    id: 2,
+    title: 'Do you know the muffin man?',
+    info:
+      'The one on drury lane?',
+  },
+  {
+    id: 3,
+    title: 'Are you a beaver? Cuz damn.',
+    info:
+      'Seriously?',
+  },
+  {
+    id: 4,
+    title: 'Is cereal soup?',
+    info:
+      'Umm, no?',
+  },
+  {
+    id: 5,
+    title: 'How come our nose runs but our feet smell?',
+    info:
+      'Wait what?',
+  },
+]
+export default questions
