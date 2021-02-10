9ca8acba61123515c25a24c9bc7e9d3cac239e22
Stephanie Raymos <stephanieraymos@gmail.com>
2021-02-09 19:34:33 -0800

    Return fixed

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import SingleQuestion from './Question';
 function App() {
   const [questions, setQuestions] = useState(data)
 
-  return <main>
-    <div className="container"><h3>Questions and Answers</h3>
-      <section className="info">
-        {
-          questions.map((question) => {
-            return <SingleQuestion key={question.id} {...question} />
-          })
-        }
-      </section>
-    </div>
-  </main>
-    ;
+  return (
+    <main>
+      <div className="container">
+        <h3>Questions and Answers</h3>
+        <section className="info">
+          {
+            questions.map((question) => {
+              return <SingleQuestion key={question.id} {...question} />
+            })
+          }
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
